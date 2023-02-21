b25dd5e66508adbdf93464f047b8311753a2c299
MIKE-BRIA <brianmichaeladero@gmail.com>
2023-02-21 08:30:21 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h'
+
+/**
+ * add - Adds two values
+ * @n1: first numb to be computed
+ * @n2: second numb to be comp
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
