08da84e9f49086a70af9a7cd6d154a1459ef0e98
MIKE-BRIA <brianmichaeladero@gmail.com>
2023-02-21 05:17:20 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet -2-print_alphabet_x10.c
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9>)
+	{
+	for(a = 'a';c <= 'z'; c++)
+	}
+	_putchar(c);
+	
+	_putchar('\n');
+	i++;
+}
+	
