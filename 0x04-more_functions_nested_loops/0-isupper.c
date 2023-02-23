007ec49bd7df90c0b7750fc185766c5bb40c15c8
MIKE-BRIA <brianmichaeladero@gmail.com>
2023-02-23 11:03:01 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - check if a letter is upper
- * @x: the number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * @x: The number to be checked
+ * Return: 1 for upper letter 0 for else
  */
 
 int _isupper(int x)
 {
 	if (x >= 65 && x <= 90)
 	{
-		return (10);
+	return (1);
 	}
 	return (0);
 }
