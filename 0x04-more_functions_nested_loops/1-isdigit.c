4fbd871f0d190e101c0e3d86a2f83384b682bbd2
MIKE-BRIA <brianmichaeladero@gmail.com>
2023-02-23 10:52:30 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,19 @@
 
 /**
  * _isdigit - check if a character is a digit
- * @c: the number to be checked
+ *@c: the number to be checked
  * Return: 1 for the character that will be a dgt or 0 for else
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
-	if (x >= 48 && x <= 57)
-	{
-	return (1);
-	}
-	return (0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
 }
+else
+{
+return (0);
+}
+}
+
