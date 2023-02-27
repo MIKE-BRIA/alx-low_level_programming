b2de8aa5b86ee4ba6b0a7b46ae8c1b6fb5ef95d7
MIKE-BRIA <brianmichaeladero@gmail.com>
2023-02-27 14:58:11 -0800

    kengen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,14 +12,15 @@ int main(void)
 
 {
 	char password[84];
-	int index = 0, sum = 0, diff_halfi, diff_half2;
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
 
 	srand(time(0));
 
 	while (sum < 2772)
 	
 	{
-		pasword[index] = 33 + rand() % 94;
+		password[index] = 33 + rand() % 94;
 		sum += password[index++];
 	}
 
